39ab58d6fa959225c9039e15718b11e9ae8f2e34
Ilfir Yakupov <philsmirnoff@gmail.com>
2024-10-22 22:06:34 -0400

    Add strings, variables, while, for loop, if statements

---
+++ HelloWorld/Strings.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class Strings {
+  public static void main(String[] args) {
+
+    String text = "Hello";
+    String blank = " ";
+    String name = "Phil";
+    String greeting = text + blank + name;
+
+    System.out.println(greeting);
+  }
+}
---
+++ HelloWorld/Variables.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class Variables {
+
+  public static void main(String[] args) {
+    int myNumber = 88;
+    short myShort = 847;
+    long myLong = 9797;
+
+    System.out.println(myLong);
+    System.out.println(myShort);
+    System.out.println(myNumber);
+    System.out.println("Hello, World.");
+  }
+
+}
